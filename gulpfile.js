046f37bbad73a71653ffca0620df0ff6054fe741
alberto <albertodeagostini88@gmail.com>
2017-12-02 20:34:27 +0100

    gulp task for css and unified bundle command

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 
+/**
+ * Bundle js in a unic file 
+ */
 // files to concatenate to create the "client" bundle
 var files = [
     './client/tetris-manager.js', 
@@ -11,8 +14,24 @@ var files = [
     './client/tetris.js', 
     './client/main.js', 
 ];
+gulp.task('bundle-js', () => {          //  TODO minify
+    return gulp.src(files)
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./server/dist/'))
+});
+
+/**
+ * Copy style
+ */
+gulp.task('copy-style', function () {
+    gulp.src('./style.css')
+        .pipe(gulp.dest('./server/dist/'));
+});
+
+
+/**
+ * Bundle js and copy style
+ */
 gulp.task('bundle', () => {
-  return gulp.src(files)
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('./server/dist/'));
+    gulp.start('bundle-js', 'copy-style');
 });
