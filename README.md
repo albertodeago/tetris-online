f54e1884cffdac5aef23a406efd576285b5d03a1
Alberto De Agostini <albertodeagostini88@gmail.com>
2022-01-31 18:25:01 +0100

    upgrade dependencies + better readme

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # tetris-online
+
 A multiplayer tetris game playable online via WebSocket with some addings to make the game a bit more funny. Toy project made to learn about websocket and because I always loved tetris.
 
 # Demo
+
 https://multiplayer-tetris-battle.herokuapp.com/index.html
 Copy paste the url (generated dinamically when you enter in the game) to a friend (on in another browser tab) to add a player to the match
+
+## Contribute
+
+To install
+`npm install`
+
+To start the dev server
+`npm run start`
+
+NOTE: There is no production build
--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,39 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 
 /**
- * Bundle js in a unic file 
+ * Bundle js in a single file 
  */
+
 // files to concatenate to create the "client" bundle
 var files = [
-    './client/tetris-manager.js', 
-    './client/connection-manager.js', 
+    './client/tetris-manager.js',
+    './client/connection-manager.js',
     './client/ux-manager.js',
-    './client/arena.js', 
-    './client/events.js', 
-    './client/player.js', 
-    './client/tetris.js', 
+    './client/arena.js',
+    './client/events.js',
+    './client/player.js',
+    './client/tetris.js',
     './client/player-control-handlers.js',
-    './client/main.js', 
+    './client/main.js',
 ];
-gulp.task('bundle-js', () => {          //  TODO minify
+gulp.task('bundle-js', () => { //  TODO minify
     return gulp.src(files)
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest('./server/dist/'))
 });
 
 /**
- * Copy style
+ * Copy styles and assets
  */
-gulp.task('copy-style', function () {
+gulp.task('copy-style', function() {
     gulp.src('./style.css')
         .pipe(gulp.dest('./server/dist/'));
 });
-gulp.task('copy-style-mobile', function () {
+gulp.task('copy-style-mobile', function() {
     gulp.src('./style-mobile.css')
         .pipe(gulp.dest('./server/dist/'));
 });
-gulp.task('copy-style-animations', function () {
+gulp.task('copy-style-animations', function() {
     gulp.src('./style-animations.css')
         .pipe(gulp.dest('./server/dist/'));
 });
@@ -46,12 +47,13 @@ gulp.task('copy-images', function() {
 /**
  * Bundle js and copy style
  */
-gulp.task('bundle', () => {
-    gulp.start(
-        'bundle-js', 
-        'copy-style', 
-        'copy-style-mobile', 
+gulp.task('bundle', (done) => {
+    gulp.series(
+        'bundle-js',
+        'copy-style',
+        'copy-style-mobile',
         'copy-style-animations',
         'copy-images'
     );
+    done()
 });
